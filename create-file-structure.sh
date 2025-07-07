91e69b300c43d5cf878918dfcd6d252612f66971
idAndrEk <AndrEkat96@yandex.ru>
2025-07-07 07:34:57 +0000

    add create-file-structure.sh

---
+++ create-file-structure.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# level-1-basics
+touch level-1-basics/01-hello-world.sh \
+      level-1-basics/02-hello-name.sh \
+      level-1-basics/03-print-user.sh \
+      level-1-basics/04-print-pwd.sh \
+      level-1-basics/05-greet-arg.sh
+
+# level-2-files
+touch 2-files/06-create-hello-txt.sh \
+      2-files/07-check-file.sh \
+      2-files/08-read-file.sh \
+      2-files/09-append-log.sh \
+      2-files/10-delete-temp.sh
+
+# conditionals
+touch 3-conditionals/11-check-number.sh \
+      3-conditionals/12-check-backup-dir.sh \
+      3-conditionals/13-compare-two-args.sh \
+      3-conditionals/14-check-readable.sh \
+      3-conditionals/15-check-root.sh
+
+# loops
+touch 4-loops/16-print-1-10.sh \
+      4-loops/17-list-files.sh \
+      4-loops/18-factorial.sh \
+      4-loops/19-create-multiple-files.sh \
+      4-loops/20-password-prompt.sh
+
+# functions
+touch 5-functions/21-greet-function.sh \
+      5-functions/22-menu-select.sh \
+      5-functions/23-login-password-check.sh \
+      5-functions/24-count-lines.sh \
+      5-functions/25-reverse-args.sh
+
+# advanced
+touch 6-advanced/26-copy-txt-to-backup.sh \
+      6-advanced/27-backup-folder.sh \
+      6-advanced/28-check-and-start-nginx.sh \
+      6-advanced/29-check-disk-usage.sh \
+      6-advanced/30-process-monitor.sh
